dff9d7b89477745f9fbdf7602f15463dd938cecd
Tamyeuem <114336806+lolsandbox@users.noreply.github.com>
2023-06-24 15:53:21 +0700

    Update anhnhoem.lua

--- anhnhoem.lua
+++ anhnhoem.lua
@@ -1,25 +1,13 @@
-repeat wait() until game:IsLoaded() and game.Players.LocalPlayer
-game.StarterGui:SetCore("SendNotification", {
-                            Title = "Notification", 
-                            Text = "Thank for use  Rivai Hub" ,
-                            Icon = "https://create.roblox.com/dashboard/creations/marketplace/13818720844/configure",
-                            Duration = 3.5
-                        })    
+repeat wait() until game:IsLoaded() 
 local PlaceId = game.PlaceId
 if PlaceId == 2753915549 or PlaceId == 4442272183 or PlaceId == 7449423635 then
     loadstring(game:HttpGet('https://raw.githubusercontent.com/TranPhatAn/Rivai-hub/main/BloxFruitsRivaiHub.source'))()
-end
-if PlaceId == 10464237910 or game.PlaceId == 13373351184 then
+elseif PlaceId == 10464237910 or PlaceId == 13373351184 then
     loadstring(game:HttpGet('https://raw.githubusercontent.com/lolsandbox/DragonSea/main/Dragon%20Sea.txt'))()
-end
-if PlaceId == 3440262694 then 
+elseif PlaceId == 3440262694 then 
     loadstring(game:HttpGet('https://raw.githubusercontent.com/lolsandbox/AirportTycoon/main/Airport%20Tycoon.txt'))()
-end
-if PlaceId == 6516141723 or game PlaceId == 6839171747 then
+elseif PlaceId == 6516141723 or PlaceId == 6839171747 then
     loadstring(game:HttpGet('https://raw.githubusercontent.com/lolsandbox/Door/main/door%20series%20x.lua'))()
-end
-if PlaceId == 537413528 then 
+elseif PlaceId == 537413528 then 
     loadstring(game:HttpGet('https://raw.githubusercontent.com/lolsandbox/BuildABoatForTreasure/main/BuildABoatForTreasure.lua'))()
 end
-else 
- game.Players.LocalPlayer:Kick("Game Not Support")
